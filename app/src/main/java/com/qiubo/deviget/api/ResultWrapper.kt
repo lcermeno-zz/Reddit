a1f82d4bbfc22256fc312a07e8e77782c47f8888
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2020-01-26 16:00:38 -0300

    added class to manage the retrofit responses

---
+++ app/src/main/java/com/qiubo/deviget/api/ResultWrapper.kt
@@ -0,0 +1,9 @@
+package com.qiubo.deviget.api
+
+import com.qiubo.deviget.responses.ErrorResponse
+
+sealed class ResultWrapper<out T> {
+    data class Success<out T>(val value: T): ResultWrapper<T>()
+    data class GenericError(val code: Int? = null, val error: ErrorResponse? = null): ResultWrapper<Nothing>()
+    object NetworkError: ResultWrapper<Nothing>()
+}
