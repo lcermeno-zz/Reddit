2168e740e59fcd430957818de609f979a6a93db5
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2020-01-26 15:48:02 -0300

    added APIClient class

---
+++ app/src/main/java/com/qiubo/deviget/api/ApiClient.kt
@@ -0,0 +1,23 @@
+package com.qiubo.deviget.api
+
+import com.qiubo.deviget.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private val public by lazy {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val builderClient = OkHttpClient.Builder()
+        builderClient.addInterceptor(interceptor)
+
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.API_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(builderClient.build())
+            .build()
+    }
+}
