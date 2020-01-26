e026e038962e1a72adbcbe4c8c3e7853b711f744
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2020-01-26 20:40:17 -0300

    added new models to parse the post response

--- app/src/main/java/com/qiubo/deviget/api/Models.kt
+++ app/src/main/java/com/qiubo/deviget/api/Models.kt
@@ -1,8 +1,29 @@
 package com.qiubo.deviget.api
 
-// responses
-data class ErrorResponse(val message: String)
-data class GetTopResponse(val error: String)
+import com.google.gson.annotations.SerializedName
+
 
 // requests
 data class GetTopRequest(val after: String)
+
+// responses
+data class ErrorResponse(val message: String)
+
+data class GetTopResponse(val data: RedditData)
+
+data class RedditData(val children: List<RedditChild>, val after: String)
+
+data class RedditChild(val data: RedditChildData)
+
+data class RedditChildData(
+    @SerializedName("author")
+    val author: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("created_utc")
+    val date: Long,
+    @SerializedName("thumbnail")
+    val thumbnailUrl: String,
+    @SerializedName("num_comments")
+    val commentQty: Int
+)
