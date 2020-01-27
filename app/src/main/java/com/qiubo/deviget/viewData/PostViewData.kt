ea5cc46a388cbad5bc35f4078568bed7eeaa72c7
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2020-01-26 23:17:03 -0300

    Updates - updated string properties like comments and time in toViewData - added BindingAdapter to load thumbnail Url

--- app/src/main/java/com/qiubo/deviget/viewData/PostViewData.kt
+++ app/src/main/java/com/qiubo/deviget/viewData/PostViewData.kt
@@ -1,14 +1,64 @@
 package com.qiubo.deviget.viewData
 
+import android.graphics.drawable.Drawable
+import android.util.Log
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.qiubo.deviget.api.RedditChild
+import org.ocpsoft.prettytime.PrettyTime
+import java.util.*
 
 data class PostViewData(
+    val id: String,
     val title: String,
     val author: String,
-    val date: Long,
+    val date: String,
     val thumbnailUrl: String,
-    val commentQty: Int,
+    val commentQty: String,
     val seen: Boolean = false
 )
 
-fun RedditChild.toViewData() = PostViewData(data.title, data.author, data.date, data.thumbnailUrl, data.commentQty)
+fun RedditChild.toViewData(prettyTime: PrettyTime) = PostViewData(
+    data.id,
+    data.title,
+    data.author,
+    prettyTime.format(Date(data.date * 1000)),
+    data.thumbnailUrl,
+    "${data.commentQty} comments"
+)
+
+@BindingAdapter("imageUrl")
+fun loadImage(view: ImageView, url: String) {
+    Glide
+        .with(view.context)
+        .load(url)
+        .listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                Log.e("PostViewData", "Image Load failed: ${e?.message}")
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                Log.d("PostViewData", "Image Loaded")
+                return false
+            }
+
+        })
+        .into(view)
+}
