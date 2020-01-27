cb24e6ad3f58f036117c05cfe0640fc774e15c5f
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2020-01-26 23:15:41 -0300

    added Post item view holder

---
+++ app/src/main/java/com/qiubo/deviget/ui/viewholders/PostViewHolder.kt
@@ -0,0 +1,6 @@
+package com.qiubo.deviget.ui.viewholders
+
+import androidx.recyclerview.widget.RecyclerView
+import com.qiubo.deviget.databinding.ItemPostMediaBinding
+
+class PostViewHolder(val binding: ItemPostMediaBinding) : RecyclerView.ViewHolder(binding.root)
