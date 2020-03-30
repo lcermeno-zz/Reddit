6bb1d9bce21cab24bd73aa1875f5b1125ea569b5
Lawrence Cermeno <lcermeno.salcedo@gmail.com>
2020-03-30 18:41:11 -0300

    simplified architecture

--- app/src/main/java/com/qiubo/deviget/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/qiubo/deviget/viewmodels/MainViewModel.kt
@@ -1,24 +1,29 @@
 package com.qiubo.deviget.viewmodels
 
 import android.util.Log
-import android.util.SparseArray
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import com.qiubo.deviget.api.ApiClient
+import com.qiubo.deviget.api.ErrorResponse
 import com.qiubo.deviget.api.GetTopRequest
-import com.qiubo.deviget.repositories.RedditRepository
-import com.qiubo.deviget.usecases.GetTopUseCase
+import com.qiubo.deviget.api.ResultWrapper
+import com.qiubo.deviget.events.GetTopEvent
 import com.qiubo.deviget.viewData.PostViewData
 import com.qiubo.deviget.viewData.toViewData
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.ocpsoft.prettytime.PrettyTime
+import retrofit2.HttpException
+import java.io.IOException
 import java.util.*
 import kotlin.collections.HashMap
 
 class MainViewModel : ViewModel() {
-
-    private var getTopUseCase: GetTopUseCase = GetTopUseCase(RedditRepository())
 
     private val _progress = MutableLiveData<Boolean>()
     private val _posts = MutableLiveData<List<PostViewData>>()
@@ -42,7 +47,7 @@ class MainViewModel : ViewModel() {
 
     fun fetchPosts(request: GetTopRequest? = null) = viewModelScope.launch {
         _progress.postValue(true)
-        val event = getTopUseCase(request)
+        val event = fetchTopEntries(request)
         when (event.success) {
             true -> {
                 _after = event.getTopResponse?.data?.after
@@ -81,6 +86,58 @@ class MainViewModel : ViewModel() {
 
     fun markSeenPost(postViewData: PostViewData) {
         _seenPosts[postViewData.id] = true
+    }
+
+    // networking
+
+    suspend fun <T> safeApiCall(
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        apiCall: suspend () -> T
+    ): ResultWrapper<T> {
+        return withContext(dispatcher) {
+            try {
+                ResultWrapper.Success(apiCall.invoke())
+            } catch (throwable: Throwable) {
+
+                throwable.message?.let { Log.e("Repository", it) }
+
+                when (throwable) {
+                    is IOException -> ResultWrapper.NetworkError
+                    is HttpException -> {
+                        val code = throwable.code()
+                        val errorResponse = convertErrorBody(throwable)
+                        ResultWrapper.GenericError(code, errorResponse)
+                    }
+                    else -> {
+                        ResultWrapper.GenericError(null, null)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun convertErrorBody(throwable: HttpException): ErrorResponse? {
+        return try {
+            throwable.response()?.errorBody()?.toString()?.let {
+                Gson().fromJson(it, ErrorResponse::class.java)
+            }
+        } catch (exception: Exception) {
+            null
+        }
+    }
+
+    // web service
+
+    suspend fun fetchTopEntries(request: GetTopRequest?): GetTopEvent {
+        return when (val response =
+            safeApiCall { ApiClient.getRedditService().fetchTop(request?.after) }) {
+            is ResultWrapper.Success -> GetTopEvent(response.value).apply { success = true }
+            is ResultWrapper.GenericError -> GetTopEvent().apply {
+                message = response.error?.message
+            }
+            is ResultWrapper.NetworkError -> GetTopEvent().apply { message = "Network Error" }
+        }
+
     }
 
 }
